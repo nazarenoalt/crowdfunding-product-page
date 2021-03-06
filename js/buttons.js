a00d1b7ad7306f16a8383b56669977718cb5b936
Nazareno Altamiranda <nazalt256@gmail.com>
2021-03-06 18:11:17 -0300

    chaning method when elements display self

--- js/buttons.js
+++ js/buttons.js
@@ -2,16 +2,20 @@ const success_button = document.querySelector('#success_button')
 const successful_back = document.querySelector('#successful_back')
 const back_project = document.querySelector('.card.back-project');
 function displayNone(element) {
-    element.style.display = 'none';
     background.style.display = 'none';
+    element.classList.remove('active-section')
 }
 
 function displayBackProject(element) {
-    element.style.display = 'block';
+    element.classList.add('active-section')
+
+    console.log(element.classList.value);
     back_project.style.display = 'none';
+    back_project.classList.remove('active-section');
 }
 
 function displayBlock(element) {
-    element.style.display = 'block';
+    element.classList.add('active-section')
+    console.log(element.classList.value);
     background.style.display = 'block';
 }
