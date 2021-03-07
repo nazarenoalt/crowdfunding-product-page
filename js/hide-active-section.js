45281b64f931ba914795d88589f5efa03bc59215
Nazareno Altamiranda <nazalt256@gmail.com>
2021-03-07 04:38:50 -0300

    adding close-menu features

--- js/hide-active-section.js
+++ js/hide-active-section.js
@@ -2,4 +2,6 @@ const hideActiveSection = () => {
     const element = document.querySelector('.active-section');
     element.classList.remove('active-section');
     background.style.display = 'none';
+
+    if(element === menu_container) menu_icon.src = '../images/icon-hamburger.svg';
 }
--- js/menu.js
+++ js/menu.js
@@ -7,12 +7,12 @@ menu_icon.addEventListener('click', function() {
     if(turned === false) {
         menu_icon.src = '../images/icon-close-menu.svg';
         background.style.display = 'block';
-        menu_container.style.display = 'block';
+        menu_container.classList.add('active-section')
         turned = true;
     } else {
         menu_icon.src = '../images/icon-hamburger.svg';
         background.style.display = 'none';
-        menu_container.style.display = 'none';
+        menu_container.classList.remove('active-section')
         turned = false;
     }
 })
